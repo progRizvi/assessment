961ce7d5454f89ba33f7849fb2cf10f8e5044484
progRizvi <prog.rizvi@gmail.com>
2023-10-14 11:43:36 +0600

    migration file added

---
+++ database/migrations/2023_10_14_054047_create_transections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transections', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("user_id")->unsigned();
+            $table->enum('type', ['deposit', 'withdrawal']);
+            $table->double('amount');
+            $table->decimal("fee", 8, 2)->default(0);
+            $table->date("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transections');
+    }
+};
